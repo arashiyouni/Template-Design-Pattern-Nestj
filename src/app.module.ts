4014734be1bba4f1099477ee1af68bf60ed670d7
angie espinoza <angie.espinoza.campos@gmail.com>
2024-10-11 16:13:01 -0600

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DependencyInyectionModule } from './dependency-inyection/dependency-inyection.module';
+import { DatabaseModule } from './database/database/database.module';
+import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DependencyInyectionModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
